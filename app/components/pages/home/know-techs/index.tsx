cc7e95703ec3c340f6fd7d3f7543345dd72f8a4f
Lucas Angeli <jl.angeli@outlook.com>
2023-09-20 17:16:05 -0300

    feat: created the knowlodge page and added tech-cards

--- app/components/pages/home/know-techs/index.tsx
+++ app/components/pages/home/know-techs/index.tsx
@@ -1,3 +1,24 @@
+import { SectionTitle } from "@/app/components/section-title";
+import { TechCard } from "./tech-card";
+import { TbBrandNextjs } from "react-icons/tb";
+
 export const KnowTechs = () => {
-  return <section className="container py-16"></section>;
+  return (
+    <section className="container py-16">
+      <SectionTitle title="Conhecimentos" subtitle="competências" />
+
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <TechCard
+            key={index}
+            tech={{
+              icon: <TbBrandNextjs />,
+              name: "Next.JS",
+              startDate: "2022-10-22",
+            }}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
