62cdbf1fd24940826211b11d49abae6e0b03ecbd
Lucas Angeli <jl.angeli@outlook.com>
2023-09-21 20:07:22 -0300

    fix: Added placeholder image on header logo

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -19,7 +19,12 @@ export const Header = () => {
     <header className="absolute top-8 w-full z-10 h-24 flex items-center justify-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <Image width={58} height={49} src="/images/logo.svg" alt="Logo" />
+          <Image
+            width={58}
+            height={49}
+            src="https://placehold.co/58x49/png"
+            alt="Logo"
+          />
         </Link>
 
         <nav className="flex items-center gap-4 sm:gap-10">
