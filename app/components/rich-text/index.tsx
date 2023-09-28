78e1b69a087d61126e6be7314efb3aa7e747b2db
Lucas Angeli <jl.angeli@outlook.com>
2023-09-28 20:29:43 -0300

    refactor: changed bold font render from graphcms

--- app/components/rich-text/index.tsx
+++ app/components/rich-text/index.tsx
@@ -4,5 +4,14 @@ import { ComponentProps } from "react";
 type RichTextProps = ComponentProps<typeof CMSRichText>;
 
 export const RichText = ({ ...props }: RichTextProps) => {
-  return <CMSRichText {...props} />;
+  return (
+    <CMSRichText
+      {...props}
+      renderers={{
+        bold: ({ children }) => (
+          <b className="text-gray-50 font-medium">{children}</b>
+        ),
+      }}
+    />
+  );
 };
