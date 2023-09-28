15088453774fb6eaa31605057edd4ee125080398
Lucas Angeli <jl.angeli@outlook.com>
2023-09-28 19:52:58 -0300

    feat:added function to get data from hygraph fetch

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,40 @@ import { HeroSection } from "./components/pages/home/hero-section";
 import { HighlightedProjects } from "./components/pages/home/highlighted-projects";
 import { KnowTechs } from "./components/pages/home/know-techs";
 import { WorkExperience } from "./components/pages/home/work-experience";
+import { fetchHygraphQuery } from "./utils/fetch-higraph-query";
+
+const getPageData = async () => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `;
+
+  return fetchHygraphQuery(query);
+};
 
 export default async function Home() {
+  const response = await getPageData();
+  console.log(response);
   return (
     <>
       <HeroSection />
