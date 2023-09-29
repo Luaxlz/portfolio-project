eb5352cb87ca17b3104bb3cd48923ef20da231c7
Lucas Angeli <jl.angeli@outlook.com>
2023-09-29 14:08:26 -0300

    refactor: reworked the card to use hygraph data

--- app/components/pages/home/work-experience/experience-card.tsx
+++ app/components/pages/home/work-experience/experience-card.tsx
@@ -1,7 +1,47 @@
+import { RichText } from "@/app/components/rich-text";
 import { TechBadge } from "@/app/components/tech-badge";
+import { WorkExperience } from "@/app/types/work-experience";
+import { differenceInMonths, differenceInYears, format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { difference } from "next/dist/build/utils";
 import Image from "next/image";
 
-export const ExperienceCard = () => {
+type ExperienceCardProps = {
+  experience: WorkExperience;
+};
+
+export const ExperienceCard = ({ experience }: ExperienceCardProps) => {
+  const {
+    companyLogo,
+    companyName,
+    companyUrl,
+    role,
+    endDate,
+    description,
+    technologies,
+  } = experience;
+
+  const startDate = new Date(experience.startDate);
+  const endDateToCalc = endDate ? new Date(endDate) : new Date();
+
+  const months = differenceInMonths(endDateToCalc, startDate);
+  const years = differenceInYears(endDateToCalc, startDate);
+  const monthsRemaining = months % 12;
+
+  const formattedStartDate = format(startDate, "MMM yyyy", { locale: ptBR });
+  const formattedEndDate = endDate
+    ? format(new Date(endDate), "MMM yyyy", { locale: ptBR })
+    : "o momento";
+
+  const formattedWorkTime =
+    years > 0
+      ? `${years} ano${years > 1 ? "s" : ""}${
+          monthsRemaining > 0
+            ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? "es" : ""}`
+            : ""
+        }`
+      : `${months} mes${months > 1 ? "es" : ""}`;
+
   return (
     <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
       <div className="flex flex-col items-center gap-4">
@@ -9,8 +49,8 @@ export const ExperienceCard = () => {
           <Image
             width={40}
             height={40}
-            alt="Logo da Empresa"
-            src="https://placehold.co/40x40/png"
+            alt={`Logo da empresa ${companyName}`}
+            src={companyLogo.url}
             className="rounded-full"
           />
         </div>
@@ -21,37 +61,30 @@ export const ExperienceCard = () => {
       <div>
         <div className="flex flex-col gap-2 text-sm sm:text-base">
           <a
-            href=""
+            href={companyUrl}
             target="_blank"
             className="text-gray-500 hover:text-emerald-500 transition-colors"
           >
-            @ Empresa
+            @ {companyName}
           </a>
-          <h4 className="text-gray-300">Cargo</h4>
+          <h4 className="text-gray-300">{role}</h4>
           <span className="text-gray-500">
-            out 2022 • O momento • (11 meses)
+            {formattedStartDate} • {formattedEndDate} • (11 meses)
           </span>
-          <p className="text-gray-400">
-            {/* Descrição da vaga */}
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
-            similique, quis inventore consectetur harum vero nobis tempora.
-            Earum quam, maiores eos sapiente at voluptates blanditiis nesciunt
-            consectetur harum soluta, corporis hic totam debitis! Laborum
-            architecto voluptatum veritatis vel esse sequi similique eaque
-            inventore nihil, eos doloremque praesentium, corrupti, nulla
-            dolores?
-          </p>
+          <div className="text-gray-400">
+            <RichText content={description.raw} />
+          </div>
         </div>
         <p className="text-gray-400 text-sm mb-3 mt-6 font-semibold">
           Competências
         </p>
         <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
-          <TechBadge name="React" />
-          <TechBadge name="React" />
-          <TechBadge name="React" />
-          <TechBadge name="React" />
-          <TechBadge name="React" />
-          <TechBadge name="React" />
+          {technologies.map((technology) => (
+            <TechBadge
+              key={`experience-${companyName}-tech-${technology.name}`}
+              name={technology.name}
+            />
+          ))}
         </div>
       </div>
     </div>
