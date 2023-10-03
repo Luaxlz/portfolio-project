f173bc782031bee92c1b5bdffbe73370de2a30a4
Lucas Angeli <jl.angeli@outlook.com>
2023-10-03 13:57:52 -0300

    feat: imported data query from projects page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,5 +1,29 @@
 import { ProjectSections } from "@/app/components/pages/project/project-sections";
 import { ProjectDetails } from "../../components/pages/project/project-details";
+import { ProjectsPageData } from "@/app/types/page-info";
+import { fetchHygraphQuery } from "@/app/utils/fetch-higraph-query";
+
+const getPageData = async (): Promise<ProjectsPageData> => {
+  const query = `
+    query ProjectsQuery {
+      projects {
+        shortDescription
+        slug
+        title
+        thumbnail {
+          url
+        }
+        technologies {
+          name
+        }
+      }
+    }`;
+
+  return fetchHygraphQuery(
+    query,
+    60 * 60 * 24 //24 hours
+  );
+};
 
 export default function Project() {
   return (
