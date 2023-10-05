97f8c377c77b9d0847538c97f4c8a408712f9083
Lucas Angeli <jl.angeli@outlook.com>
2023-10-05 16:43:41 -0300

    feat: added animation to the hero-section page

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -6,6 +6,7 @@ import { TechBadge } from "@/app/components/tech-badge";
 import { HomePageInfo } from "@/app/types/page-info";
 import Image from "next/image";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 type HeroSectionProps = {
   homeInfo: HomePageInfo;
@@ -36,7 +37,13 @@ export const HeroSection = ({ homeInfo }: HeroSectionProps) => {
     "
     >
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
-        <div className="w-full lg:max-w-[530px]">
+        <motion.div
+          className="w-full lg:max-w-[530px]"
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+        >
           <p className="font-mono text-emerald-400">Olá, meu nome é</p>
           <h2 className="text-4xl font-medium mt-2">Lucas Angeli</h2>
 
@@ -46,7 +53,14 @@ export const HeroSection = ({ homeInfo }: HeroSectionProps) => {
 
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
             {homeInfo.technologies.map((tech, i) => (
-              <TechBadge key={i} name={tech.name} />
+              <TechBadge
+                key={`intro-tech-${tech.name}`}
+                name={tech.name}
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                transition={{ duration: 0.5, delay: i * 0.1 }}
+              />
             ))}
           </div>
 
@@ -69,15 +83,23 @@ export const HeroSection = ({ homeInfo }: HeroSectionProps) => {
               ))}
             </div>
           </div>
-        </div>
+        </motion.div>
 
-        <Image
-          width={420}
-          height={404}
-          src={homeInfo.profilePicture.url}
-          alt="Foto de perfil do Lucas Angeli"
-          className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
-        />
+        <motion.div
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: 200, scale: 0.5 }}
+          transition={{ duration: 0.5 }}
+          className="origin-center"
+        >
+          <Image
+            width={420}
+            height={404}
+            src={homeInfo.profilePicture.url}
+            alt="Foto de perfil do Lucas Angeli"
+            className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
+          />
+        </motion.div>
       </div>
     </section>
   );
